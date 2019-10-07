a43d7852efc9c415bee371ec49501d6ec1a63e1f
Adam Sobotka <vorcigernix@gmail.com>
2019-10-07 17:00:29 +0200

    cleanup

--- src/routes/product/index.svelte
+++
@@ -1,7 +0,0 @@
-<script context="module">
-  export async function preload(page, session) {
-    const data = await this.fetch('product.json');
-    console.log(data);
-    return { data };
-  }
-</script>
