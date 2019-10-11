f8643ee7fbfc3ba8734a57dee0f7f4d99460d95f
Adam Sobotka <vorcigernix@gmail.com>
2019-10-11 14:06:21 +0200

    intersection

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,31 @@
 <script>
-	import HeroBanner from '../components/Banner.svelte';
-	import Overview from '../components/Overview.svelte';
-	import OpenHours from '../components/OpenHours.svelte';
-	import { fade } from 'svelte/transition';
-
+  import HeroBanner from "../components/Banner.svelte";
+  import Overview from "../components/Overview.svelte";
+  import OpenHours from "../components/OpenHours.svelte";
+  import { fade } from "svelte/transition";
+  import IntersectionObserver from "../components/Intersection.svelte";
 </script>
 
 <style>
+
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Sapper project template</title>
 </svelte:head>
 
-<div transition:fade>
-	<HeroBanner bannerSubtitle="You should come and taste for yourself" bannerTitle="Welcome" themeBannerHeight='hero-banner--full-height' hasButton=true bannerImg="hero-banner--homepage"/>
-	<Overview />
-	<OpenHours />
+<div>
+  <HeroBanner
+    bannerSubtitle="You should come and taste for yourself"
+    bannerTitle="Welcome"
+    themeBannerHeight="hero-banner--full-height"
+    hasButton="true"
+    bannerImg="hero-banner--homepage" />
+  <IntersectionObserver once let:intersecting top={400}>
+    {#if intersecting}
+		<div>fkin observer </div>
+    {/if}
+  </IntersectionObserver>
+      <Overview />
+      <OpenHours />
 </div>
